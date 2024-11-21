ba0244d86d5dc0b95be9569b52e58f0e8393691c
Hasan Eke <hasaneke1000@gmail.com>
2024-11-22 02:10:38 +0300

    fix prop drilling, refactor with contextapi

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -1,6 +1,7 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
+import {forwardRef, useContext, useImperativeHandle, useRef} from 'react';
 import { createPortal } from 'react-dom';
 import Cart from './Cart';
+import {CartContext} from "../store/shopping-cart-context.jsx";
 
 const CartModal = forwardRef(function Modal(
   { cartItems, onUpdateCartItemQuantity, title, actions },
@@ -19,7 +20,7 @@ const CartModal = forwardRef(function Modal(
   return createPortal(
     <dialog id="modal" ref={dialog}>
       <h2>{title}</h2>
-      <Cart items={cartItems} onUpdateItemQuantity={onUpdateCartItemQuantity} />
+      <Cart />
       <form method="dialog" id="modal-actions">
         {actions}
       </form>
